d9018c2bdd51d3662d894bbd93db62c50dd34470
Emherson Borillo <thesoundhaven@gmail.com>
2017-12-12 18:49:01 -1000

    Add files via upload

---
+++ helpers/user_data.php
@@ -0,0 +1,57 @@
+<?php
+	function get_user_data()
+	{
+		$user_array = array
+		(
+			"guest" 		=> "spectate",
+			"hokupaa" 		=> "z1gZ@g",
+			"iwakelii" 		=> "j0ck3y",
+			"nahiku" 		=> "bL@z3d",
+			"hokulei" 		=> "\$phYnx",
+			"deneb" 		=> "h@zArd",
+			"keoe" 			=> "V3x!ng",
+			"nanamua" 		=> "Qu@h0g",
+			"hokulea" 		=> "b@ckUp",
+			"regulus" 		=> "iNKj3t",
+			"humu" 			=> "Pr3f!x",
+			"kauluakoko" 	=> "\$qu3ak",
+			"puana" 		=> "DeF3ct",
+			"puanakau" 		=> "J!nx3d",
+			"hikianalia" 	=> "w!z@rD",
+			"aa" 			=> "3Xp3ct",
+			"lehuakona" 	=> "wH@Mmy",
+			"kamailemua" 	=> "d3Fu\$e",
+			"kamolehonua" 	=> "!c3b0x",
+			"admin" => "hats.team"
+		);
+		return $user_array;
+	}
+
+	function get_user_match()
+	{
+		$user_match = array
+		(
+			"guest" 		=> "guest",
+			"hokupaa" 		=> "team1",
+			"iwakelii" 		=> "team2",
+			"nahiku" 		=> "team3",
+			"hokulei" 		=> "team4",
+			"deneb" 		=> "team5",
+			"keoe" 			=> "team6",
+			"nanamua" 		=> "team7",
+			"hokulea" 		=> "team8",
+			"regulus" 		=> "team9",
+			"humu" 			=> "team10",
+			"kauluakoko" 	=> "team11",
+			"puana" 		=> "team12",
+			"puanakau" 		=> "team13",
+			"hikianalia" 	=> "team14",
+			"aa" 			=> "team15",
+			"lehuakona" 	=> "team16",
+			"kamailemua" 	=> "team17",
+			"kamolehonua" 	=> "team18",
+			"admin" => "administrator"
+		);
+		return $user_match;
+	}
+?>
